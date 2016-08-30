e79fb36310fc4ccb94129c558c8295bf6b10ed53
S Mirunalini <mirunalinirajan@gmail.com>
2016-08-30 20:39:27 +0530

    Add files via upload

---
+++ player24.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class player24
+{
+	public static void main(String [] args)
+	{
+		Scanner S=new Scanner(System.in);
+		System.out.println("Enter the string");
+		String s=S.next();
+		char a[]=s.toCharArray();
+		if(isDigit(ch[0]) && isAlphabetic(ch.length-1))
+		{
+			System.out.println("False");
+		}
+		else
+		{
+			System.out.println("True");
+		}
+	}
+}
