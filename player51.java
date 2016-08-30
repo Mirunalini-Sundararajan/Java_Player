89bbffcc67853d8cf71e6df94fdb090efd5b9b15
S Mirunalini <mirunalinirajan@gmail.com>
2016-08-30 20:51:10 +0530

    Add files via upload

---
+++ player51.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class player51
+{
+	public static void main(String [] args)
+	{
+		Scanner S=new Scanner(System.in);
+		System.out.println("Enter the array count");
+		int n=S.nextInt();
+		System.out.println("Enter the array elements");
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=S.nextInt();	
+		}
+		Arrays.sort(a);
+		System.out.println("Second smallest element entered is "+a[1]);
+	}
+}
